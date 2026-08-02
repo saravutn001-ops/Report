977c517d85e6afbd182e6d671b8c34bfac4a6cb9
saravutn001-ops <saravut.n001@gmail.com>
2026-08-02 10:20:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.PRODCLOSE_CONFIG = {
   // https://script.google.com/macros/s/AKfycbz3kTICa9jV_-x-BEmoFmF-IY3nJ2RMIn3l1W5GVujAGNCuNwTOQDVxjqZe5b2FAIYpNg/exec
-  apiUrl: "PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE",
+  apiUrl: "https://script.google.com/macros/s/AKfycbz3kTICa9jV_-x-BEmoFmF-IY3nJ2RMIn3l1W5GVujAGNCuNwTOQDVxjqZe5b2FAIYpNg/exec",
   appName: "ProdClose 1400"
 };
