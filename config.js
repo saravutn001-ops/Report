6b1f3b49b9257b64e9bca9c5071f51f62bc664e2
saravutn001-ops <saravut.n001@gmail.com>
2026-08-16 08:55:36 +0700

    Update apiUrl in configuration

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.PRODCLOSE_CONFIG = {
-  apiUrl: "PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE",
+  apiUrl: "https://script.google.com/macros/s/AKfycbwTgatvGI4tWZ1PcD968Zhq3ytzsILH9Ql-2BaXVtPTqOyHY0kkepREiViUREkraf5AfA/exec",
   appName: "ProdClose 1400"
 };
