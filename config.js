93f39e4bf9872d1e6d4fd9d463c5db8b91937d57
saravutn001-ops <saravut.n001@gmail.com>
2026-08-16 08:51:33 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 window.PRODCLOSE_CONFIG = {
-  // https://script.google.com/macros/s/AKfycbz3kTICa9jV_-x-BEmoFmF-IY3nJ2RMIn3l1W5GVujAGNCuNwTOQDVxjqZe5b2FAIYpNg/exec
-  apiUrl: "https://script.google.com/macros/s/AKfycbz3kTICa9jV_-x-BEmoFmF-IY3nJ2RMIn3l1W5GVujAGNCuNwTOQDVxjqZe5b2FAIYpNg/exec",
+  apiUrl: "PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE",
   appName: "ProdClose 1400"
 };
